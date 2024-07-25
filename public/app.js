02c2be57ac6a4c14fb3abe1d2716dc9cac91bb7d
Jpisnice <www.janardhanpolle@gmail.com>
2024-07-25 22:54:56 +0530

    added files

---
+++ public/app.js
@@ -0,0 +1,8 @@
+function showDbs(connString){
+    //Render all dbs in of that user in a tabble form
+}
+
+function compareTables(db1,db2){
+    //compare each and every db for matching schema and data types of colums if matched names of columns
+    //write 
+}
