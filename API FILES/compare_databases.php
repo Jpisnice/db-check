6da5ec3f89ad1347f04fc9f33053cde3eebb08e8
Jpisnice <www.janardhanpolle@gmail.com>
2024-07-26 13:10:52 +0530

    Final Functionality

--- API FILES/compare_databases.php
+++ API FILES/compare_databases.php
@@ -2,8 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-// Check if necessary parameters are provided
-if (!isset($_POST['username'])|| !isset($_POST['database1']) || !isset($_POST['database2'])) {
+if (!isset($_POST['username']) || !isset($_POST['password']) || !isset($_POST['database1']) || !isset($_POST['database2'])) {
     echo json_encode(["error" => "Username, password, and database names are required"]);
     exit();
 }
@@ -13,17 +12,28 @@ $password = $_POST['password'];
 $database1 = $_POST['database1'];
 $database2 = $_POST['database2'];
 
-// Database connection parameters
 $servername = "localhost";
 
-// Create connection
 $conn1 = new mysqli($servername, $username, $password, $database1);
 $conn2 = new mysqli($servername, $username, $password, $database2);
 
-// Check connection
 if ($conn1->connect_error || $conn2->connect_error) {
     echo json_encode(["error" => "Connection failed: " . $conn1->connect_error . " / " . $conn2->connect_error]);
     exit();
+}
+
+function getTables($conn, $database) {
+    $sql = "SELECT table_name FROM INFORMATION_SCHEMA.TABLES WHERE table_schema = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $database);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $tables = [];
+    while ($row = $result->fetch_assoc()) {
+        $tables[] = $row['table_name'];
+    }
+    return $tables;
 }
 
 function getTableColumns($conn, $database) {
@@ -42,32 +52,35 @@ function getTableColumns($conn, $database) {
     return $tables;
 }
 
-$tables1 = getTableColumns($conn1, $database1);
-$tables2 = getTableColumns($conn2, $database2);
+$tables1 = getTables($conn1, $database1);
+$tables2 = getTables($conn2, $database2);
+
+$matchingTables = array_intersect($tables1, $tables2);
+$missingTablesInDb1 = array_diff($tables1, $tables2);
+$missingTablesInDb2 = array_diff($tables2, $tables1);
+
+$columns1 = getTableColumns($conn1, $database1);
+$columns2 = getTableColumns($conn2, $database2);
 
 $comparisons = [];
 
-foreach ($tables1 as $table1 => $columns1) {
-    foreach ($tables2 as $table2 => $columns2) {
-        $commonFields = array_intersect_key($columns1, $columns2);
-        
-        if (!empty($commonFields)) {
-            $fieldTypes = [];
-            foreach ($commonFields as $field => $type) {
-                $fieldTypes[$field] = $type;
-            }
-            
-            $comparisons[] = [
-                "tableName1" => $table1,
-                "tableName2" => $table2,
-                "similarities" => [
-                    "commonFields" => array_keys($commonFields),
-                    "fieldTypes" => $fieldTypes,
-                    "totalSimilarFields" => count($commonFields)
-                ]
-            ];
-        }
+foreach ($matchingTables as $table) {
+    $commonFields = array_intersect_key($columns1[$table], $columns2[$table]);
+    
+    $fieldTypes = [];
+    foreach ($commonFields as $field => $type) {
+        $fieldTypes[$field] = $type;
     }
+    
+    $comparisons[] = [
+        "tableName1" => $table,
+        "tableName2" => $table,
+        "similarities" => [
+            "commonFields" => array_keys($commonFields),
+            "fieldTypes" => $fieldTypes,
+            "totalSimilarFields" => count($commonFields)
+        ]
+    ];
 }
 
 $response = [
@@ -76,6 +89,12 @@ $response = [
         [
             "dbName1" => $database1,
             "dbName2" => $database2,
+            "matchingTables" => $matchingTables,
+            "missingTables" => [
+                "inDb1" => $missingTablesInDb2,
+                "inDb2" => $missingTablesInDb1
+                
+            ],
             "tableComparisons" => $comparisons
         ]
     ]
